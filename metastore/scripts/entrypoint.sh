d4c98e150a91d1c15d02bbf936222af00b2a33b2
Nick <myfjdthink@163.com>
2023-04-13 15:27:55 +0800

    feat: init

---
+++ metastore/scripts/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export JAVA_HOME="$(jrunscript -e 'java.lang.System.out.println(java.lang.System.getProperty("java.home"));')"
+export HADOOP_HOME=/opt/hadoop
+export HIVE_HOME=/opt/metastore
+export METASTORE_HADOOP_VERSION=3.2.0
+export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-${METASTORE_HADOOP_VERSION}.jar
+
+
+# metastore
+# initSchema only needs to be run once
+#${HIVE_HOME}/bin/schematool -initSchema -dbType postgres
+${HIVE_HOME}/bin/start-metastore
